dac0b955fc81fa45dcc476ef54dc7bfa6a78e05d
Kate Leslie <thisizk8@gmail.com>
2017-02-13 16:35:10 -0600

    more tests

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -1,17 +1,9 @@
 import cardSum from './card-sum';
 
-export default function(card1, card2) {
+export default function (card1, card2) {
   if (card1 === card2) {
     return false;
   }
 
-  if (cardSum(card1, card2) < 17) {
-    return true;
-  }
-  if (cardSum(card1, card2) === 17) {
-    return true;
-  }
-  if (cardSum(card1, card2) > 17) {
-    return false;
-  }
+  return cardSum(card1, card2) <= 17;
 }
